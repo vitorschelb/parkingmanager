27ada2bfd32befb8a94af0d233fb13251b9837bf
vitorschelb <116350364+vitorschelb@users.noreply.github.com>
2023-05-01 20:21:21 -0300

    boilerplate

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,10 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
       <body>
-        <Main />
-        <NextScript />
       </body>
     </Html>
   )
